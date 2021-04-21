9b0b28a29a5b4edc515c55af5cd0e2f3b4f57ede
Alex-Valentino Georgiev <aresfrend13@gmail.com>
2021-04-21 23:50:05 +0300

    Update motionDetectionDatabase.ino
    added timestamps for detected motions and some minor adjustments to recording data in the database

--- MICROCONTROLER/motionDetectionDatabase/motionDetectionDatabase.ino
+++ MICROCONTROLER/motionDetectionDatabase/motionDetectionDatabase.ino
@@ -1,3 +1,4 @@
+#include <NTPClient.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <FirebaseESP8266.h>
@@ -14,9 +15,16 @@ FirebaseData firebaseData;
 int curr = LOW;
 int prev = LOW;
 
+//time
+const long offset = 10800;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", offset);
+  
 void setup() {
+  //pins
   pinMode(PIN_TO_SENSOR, INPUT);
   pinMode(LED_pin, OUTPUT);
+  //dataBase
   Serial.begin(115200);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
@@ -30,7 +38,9 @@ void setup() {
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
 
-//  if (Firebase.pushInt(firebaseData, "/pishkaHui", 10))
+  timeClient.begin();
+
+//  if (Firebase.pushInt(firebaseData, "/", 10))
 //    {
 //      Serial.println("PASSED");
 //      Serial.println("PATH: " + firebaseData.dataPath());
@@ -42,20 +52,32 @@ void setup() {
 //    }
 }
 
+String pathForDevice="/device";
+String timeStamp;
+String formattedTime;
 void loop() {
-  // put your main code here, to run repeatedly:
-  prev = curr; // store old state
-  curr = digitalRead(PIN_TO_SENSOR);   // read new state
-
+  //get time
+  formattedTime = timeClient.getFormattedTime();
+  int splitT = formattedTime.indexOf("T");
+  timeStamp = formattedTime.substring(splitT+1, formattedTime.length());
+  //
+  prev = curr;
+  curr = digitalRead(PIN_TO_SENSOR);
+  
   if (prev == LOW && curr == HIGH) {
     Serial.println("Motion detected!");
     digitalWrite(LED_pin, HIGH);
+    timeClient.update();
     
     FirebaseJson updateData;
-    updateData.set("boole", true);
-    if (Firebase.updateNode(firebaseData, "/", updateData))
+    updateData.set("state", true);
+    if (Firebase.updateNode(firebaseData, pathForDevice, updateData))
     {
       Serial.println("UPDATED");
+      if (Firebase.pushString(firebaseData, pathForDevice+"/timeStamps", timeStamp))
+      {
+        Serial.println("TIME STAMP:  "+timeStamp);
+      }
     }else {
       Serial.println(firebaseData.errorReason());
     }
@@ -66,8 +88,8 @@ void loop() {
     digitalWrite(LED_pin, LOW);
     
     FirebaseJson updateData;
-    updateData.set("boole", false);
-    if (Firebase.updateNode(firebaseData, "/", updateData))
+    updateData.set("state", false);
+    if (Firebase.updateNode(firebaseData, pathForDevice, updateData))
     {
       Serial.println("UPDATED");
     }else {
