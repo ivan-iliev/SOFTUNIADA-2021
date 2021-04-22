e7993c9837dfd8819193c8fee98e739c4cd2b483
Blagovest-Hristov <bobo4e.h@gmail.com>
2021-04-22 12:42:45 +0300

    Carousel Working! and Visual Updates
    Complete re-work of the carousel and a design update.

--- WEB SERVER/index.js
+++ WEB SERVER/index.js
@@ -1,15 +1,12 @@
-$(document).ready(function(){
-      $('.your-class').slick({
-        slidesToShow: 3,
-        slidesToScroll: 3
-    });
-    var slideIndex = 1;
-    $('.add').on('click', function() {
-        slideIndex++;
-        $('.your-class').slick('slickAdd','<div><h3>' + slideIndex + '</h3></div>');
-      });
-      
-
-
-    });
+$('.slider-info').slick({
+  infinite: false,
+  speed: 200,
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
+var slideIndex = 1;
+$('.js-add-slide').on('click', function() {
+  slideIndex++;
+  $('.slider-info').slick('slickAdd','<div><h3>' + slideIndex + '</h3></div>');
+});
 
