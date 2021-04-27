5cc8c12f92197ed2a5e719c417a1e0646ec33975
Alex-Valentino Georgiev <aresfrend13@gmail.com>
2021-04-27 13:03:13 +0300

    Major update(Firebase)
    Implemented firebase realtime databse in webpage; added favicon; limited the number of devices

--- WEB SERVER/index.js
+++ WEB SERVER/index.js
@@ -1,13 +1,17 @@
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyDaF6_wkqRd0vpL4Yocz1Lv8sj0oAHUFKM",
+  authDomain: "softuniada.firebaseapp.com",
+  databaseURL: "https://softuniada.firebaseio.com",
+  projectId: "softuniada",
+  storageBucket: "softuniada.appspot.com",
+  messagingSenderId: "42389912385",
+  appId: "1:42389912385:web:3b7ae948c63edb6fd07fc0",
+  measurementId: "G-Y871DLTECD"
+};
+firebase.initializeApp(firebaseConfig);
+var database=firebase.database();
 
 $('.slider-info').slick({
-
   infinite: true,
   speed: 200,
   slidesToShow: 3,
@@ -15,14 +19,24 @@ $('.slider-info').slick({
 });
 var slideIndex = 1;
 var deviceName;
+var devices=[];
 
 $('.js-add-slide').on('click', function() {
   event.preventDefault();
-  slideIndex++;
   deviceName = document.getElementById("deviceName").value;
-  $('.slider-info').slick('slickAdd','<div><h3>' + deviceName + '</h3></div>');
-  closeForm();
-  document.getElementById("deviceName").value=null;
+  if(deviceName!=""){
+    slideIndex++;
+    if(devices.length<5){
+      database.ref('devices/' + deviceName).set({
+        state: false
+      });
+      devices.push(deviceName);
+      $('.slider-info').slick('slickAdd','<div><h2>' + deviceName + '</h2></div>');
+      closeForm();
+    }else{
+      alert("You can not add a new device right now!");
+    }
+  }
 });
 
 $('.js-remove-slide').on('click', function() {
@@ -31,4 +45,3 @@ $('.js-remove-slide').on('click', function() {
     slideIndex--;
   }
 });
-
