8e29f6f8d9255872f1ff95acaae94885ed324881
Alex-Valentino Georgiev <aresfrend13@gmail.com>
2021-05-10 10:38:50 +0300

    working realtime updates with vue.js

--- WEB SERVER/index.js
+++ WEB SERVER/index.js
@@ -8,33 +8,48 @@ const firebaseConfig = {
   appId: "1:42389912385:web:3b7ae948c63edb6fd07fc0",
   measurementId: "G-Y871DLTECD"
 };
+const functions = firebase.functions;
 firebase.initializeApp(firebaseConfig);
 var database=firebase.database();
 
-Vue.use(VueCarousel);
+
 $('.slider-info').slick({
   infinite: true,
   speed: 200,
   slidesToShow: 3,
-  arrows: false,
+  arrows: false
 });
+
 var slideIndex = 1;
 var deviceName;
 var devices=[];
 var deviceStates=[];
 var stateDevice;
+var numberDevices;
+
+// function getDatabseData(){
+//   database.ref('devices').on('value',(snapshot)=>{
+//     numberDevices=snapshot.val().lenght.lenght;
+//   });
+//   for(var i=1; i<=numberDevices; i++){
+//     database.ref('devices/'+new String(i)).on('value',(snapshot)=>{
+//       devices.push(snapshot.val().name);
+//       deviceStates[i-1]=snapshot.val().state;
+//     });
+//   }
+// }
+
 function updateData(){
     for(var i=0; i<devices.length; i++){
-        database.ref('devices/' + devices[i]).on('value',(snapshot)=>{
-        devices[snapshot.val().id]=snapshot.val().name;
-        deviceStates[snapshot.val().id]=snapshot.val().state;
-        app.datalist[snapshot.val().id]=
+      database.ref('devices/' + devices[i]).on('value',(snapshot)=>{
+        devices[snapshot.val().id-1]=snapshot.val().name;
+        deviceStates[snapshot.val().id-1]=snapshot.val().state;
+        app.datalist[snapshot.val().id-1]=
         {
-          index: new Int16Array(snapshot.val().id),
-          name: new String(devices[snapshot.val().id]),
-          state: new Boolean(devices[snapshot.val().id]),
+          index: snapshot.val().id,
+          name: devices[snapshot.val().id-1],
+          state: deviceStates[snapshot.val().id-1]
         }
-        console.log(Boolean(deviceStates[snapshot.val().id]));
       });
     }
 }
@@ -42,11 +57,11 @@ function updateData(){
 var deviceName;
 $('.js-add-slide').on('click', function() {
   event.preventDefault();
-  deviceName = document.getElementById("deviceName").value;
-  devices.push(deviceName);
-  deviceStates.push(false);
   if(deviceName!=""){
-    if(devices.length<10){
+    if(devices.length<1){
+      deviceName = document.getElementById("deviceName").value;
+      devices.push(deviceName);
+      deviceStates.push(false);
         database.ref('devices/'+deviceName).set({
             id:devices.length,
             name:deviceName,
@@ -54,25 +69,25 @@ $('.js-add-slide').on('click', function() {
         });
         app.datalist.push(
           {
-            index: new Int16Array(devices.length),
-            name: new String(deviceName),
-            state: new Boolean(false),
-            style:{
-              'display': 'inline-block',
-              'background': 'rgb(37, 37, 37)',
-              'color': "#9c27b0",
-              'height': "400px",
-              'width': '23.5%',
-              'line-height': "80px",
-              'margin-bottom': "2%",
-              'margin-left': "40px",
-              'position':"relative",
-              'text-align': "center",
-              'border-radius': "5px"
-            }
+            index: devices.length,
+            name: deviceName,
+            state: false
+            // style:{
+            //   'display': 'inline-block',
+            //   'background': 'rgb(37, 37, 37)',
+            //   'color': "#9c27b0",
+            //   'height': "400px",
+            //   'width': '23.5%',
+            //   'line-height': "80px",
+            //   'margin-bottom': "2%",
+            //   'margin-left': "40px",
+            //   'position':"relative",
+            //   'text-align': "center",
+            //   'border-radius': "5px"
+            // }
           })
         console.log(Boolean(stateDevice));
-        $('.slider-info').slick('slickAdd','<div><h3>' + deviceName + '</h3></div>');
+        // $('.slider-info').slick('slickAdd','<li><h3>'+app.datalist[devices.length-1].name+'</br>'+ app.datalist[devices.length-1].state+'</h3></li>');
         closeForm();
     }else{
       alert("You can not add a new device right now!");
@@ -84,15 +99,22 @@ var app = new Vue({
     el: '#app',
     data:{
       datalist:[],
-      options: {
-        currentPage: 0,
-        infinite: 4,
-        slidesToScroll: 4,
-        loop: true
+      renderComponent: true 
+    },
+    methods: {
+      forceRerender() {
+        // Remove my-component from the DOM
+        this.renderComponent = false;
+        this.$nextTick(() => {
+          // Add the component back in
+          this.renderComponent = true;
+        });
       }
     }
+  
 });
 
 setInterval(function(){
     updateData();
-}, 10000)
+    app.forceRerender();
+}, 100)
